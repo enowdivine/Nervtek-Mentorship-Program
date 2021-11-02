5c66e968aa1cd4b117882b069212598efde4f155
becksayuk <becksayuk1@gmail.com>
2021-11-02 09:55:51 +0100

    simple calculator

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function Ans(){
+var a;
+var b;
+var sum;
+var substract;
+var multiply;
+var divide;
+var Ans;
+if(sum=a+b){
+    Ans=sum;
+alert(Ans);
+}
+if(substract=a-b){
+    Ans=substract;
+alert(Ans);
+}
+if(multiply=a*b){
+    Ans=multiply;
+alert(Ans);
+}
+if(divide=a/b){
+Ans=divide;
+alert(Ans);
+}
+}
